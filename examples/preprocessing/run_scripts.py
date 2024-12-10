c61bb6e8655a093fd4df556a293111bde2e35d04
wukan <wu-kan@163.com>
2024-12-10 18:05:35 +0800

    Update run_scripts.py

--- examples/preprocessing/run_scripts.py
+++ examples/preprocessing/run_scripts.py
@@ -1,7 +1,7 @@
 from examples import jqresearch as jqr  # noqa
 from examples import preprocessing as prp  # noqa
 
-prp.remove_tmp.main()
+prp.rename_last.main()
 
 jqr.get_all_securities.main()
 jqr.get_fundamentals_valuation.main()
