198b79c2050e1574c2a3a5f1d2524dabbc29d57d
wukan <wu-kan@163.com>
2025-09-12 23:29:44 +0800

    Update config.py

--- examples/jqresearch/config.py
+++ examples/jqresearch/config.py
@@ -1,24 +1,28 @@
 import pathlib
 
 # 使用ksrpc代替原生接口下载数据
-from ksrpc.connections.websocket import WebSocketConnection
+from ksrpc.connections.websocket import WebSocketConnection  # noqa
+from ksrpc.connections.http import HttpxConnection  # noqa
 from ksrpc.rpc_client import RpcClient
 
 TOKEN = 'secret-token-2'
-URL = 'ws://127.0.0.1:8000/ws/admin?room=9527'
-URL = 'ws://1.117.86.56:8000/ws/admin?room=9527'
-
+URL = 'ws://127.0.0.1:8000/ws/admin?room=HA9527'
 conn = WebSocketConnection(URL, token=TOKEN)
+
+URL = 'http://127.0.0.1:7001/api/file'
+conn = HttpxConnection(URL, token=TOKEN)
+
 jq = RpcClient('jqresearch.api', conn, async_local=False, async_remote=False)
 jq.cache_get = False
-jq.cache_expire = 60
+jq.cache_expire = 5
+jq.recv_timeout = 30
 
 # StateException('Interruptingcow can only be used from the MainThread.',)
 # 由于query相关操作无法异步，所以只能指定async_remote=False
 jqr = RpcClient('jqresearch_query', conn, async_local=False, async_remote=False)
 jqr.cache_get = False
-jqr.cache_expire = 60
+jqr.cache_expire = 5
+jqr.recv_timeout = 30
 
-DATA_ROOT = pathlib.Path('../..').resolve() / 'data/jqresearch'
 DATA_ROOT = pathlib.Path(r'D:\data\jqresearch')
 DATA_ROOT_AKSHARE = pathlib.Path(r'D:\data\akshare')
