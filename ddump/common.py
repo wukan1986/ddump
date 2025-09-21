bedf7e4bbba322085acb58a9b5e130ed20266f71
wukan <wu-kan@163.com>
2025-09-21 21:00:45 +0800

    还是减少每批大小，防内存崩溃

--- ddump/common.py
+++ ddump/common.py
@@ -15,7 +15,7 @@ TEMP_SUFFIX = '.tmp'  # 临时文件后缀名
 KEY_SEP_ID = '__'  # 文件名分隔符。注意:KEY中不要出现__否则无法区分。KEY中出现双下划线的概率比单下载线的概率低很多
 START_SEP_END = '__'  # 文件名分隔符。用于区分开始时间与结束时间。
 OLD_DATA_DAY = 86400 * 10  # N天前的老数据认为不再更新，不需要重复下载。最长放9天
-TEMP_DIR = pathlib.Path(tempfile.gettempdir()) / 'ddump'
+TEMP_DIR = pathlib.Path(tempfile.gettempdir()) / 'ddump' # C:\Users\Kan\AppData\Local\Temp\ddump
 TEMP_DIR.mkdir(parents=True, exist_ok=True)
 logger.info(TEMP_DIR)
 
