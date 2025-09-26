ab3d589e662c6035d0280af8c2c44a2561bab2d1
wukan <wu-kan@163.com>
2025-09-26 15:38:26 +0800

    整理缓存key的生成方式

--- ddump/common.py
+++ ddump/common.py
@@ -15,21 +15,17 @@ TEMP_SUFFIX = '.tmp'  # 临时文件后缀名
 KEY_SEP_ID = '__'  # 文件名分隔符。注意:KEY中不要出现__否则无法区分。KEY中出现双下划线的概率比单下载线的概率低很多
 START_SEP_END = '__'  # 文件名分隔符。用于区分开始时间与结束时间。
 OLD_DATA_DAY = 86400 * 10  # N天前的老数据认为不再更新，不需要重复下载。最长放9天
-TEMP_DIR = pathlib.Path(tempfile.gettempdir()) / 'ddump' # C:\Users\Kan\AppData\Local\Temp\ddump
+TEMP_DIR = pathlib.Path(tempfile.gettempdir()) / 'ddump'  # C:\Users\Kan\AppData\Local\Temp\ddump
 TEMP_DIR.mkdir(parents=True, exist_ok=True)
 logger.info(TEMP_DIR)
 
 
 def get_key(func_name, args, kwargs, ext):
     # 将参数转换为字符串
-    args_str = pickle.dumps(args)
-    kwargs_str = pickle.dumps(kwargs)
-
-    # 组合函数名和参数
-    key_str = func_name.encode("utf-8") + b':' + args_str + b':' + kwargs_str
+    d = dict(func_name=func_name, args=args, kwargs=kwargs)
 
     # 计算 MD5
-    return TEMP_DIR / (hashlib.md5(key_str).hexdigest() + ext)
+    return TEMP_DIR / (hashlib.md5(pickle.dumps(d)).hexdigest() + ext)
 
 
 def write_obj(obj, path):
