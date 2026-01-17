bbcf95afcc2054d5b64dfd391a950a7448901e4c
wukan <wu-kan@163.com>
2026-01-17 21:50:43 +0800

    重新实现PIT功能

--- examples/preprocessing/step3.py
+++ examples/preprocessing/step3.py
@@ -13,30 +13,60 @@ def load_financial(path: str) -> pl.DataFrame:
         .rename({'pubDate': 'time'})
         .drop(['id', 'statDate.1'])
         .with_columns(pl.col('time', 'statDate').str.strptime(pl.Datetime, "%Y-%m-%d"))
+        .fill_null(np.nan)  # 防止某期无值，forward_fill时产生数据跨期的问题
     )
 
 
-def adjust_financial(df: pl.DataFrame, by1: str = 'code', by2: str = 'time', by3: str = 'statDate') -> pl.DataFrame:
-    """财务报表调整。为了与交易日对齐，已经过滤了以下情况
+def pit_prepare(df: pl.DataFrame, by1: str = 'stock_code', by2: str = 'announce_date', by3: str = 'report_date') -> pl.DataFrame:
+    """将原始的财务表根据发布日期重新扩展，满足以下条件：
 
-    1. 2019年报由于疫情，比2020年一季报公布还晚
-    2. 年报和一季报常同一天出
-
-    Notes
-    -----
-    已经调整过的报表不能再进行时序上的计算和处理。因为数据已经不满足Point In Time的要求
+    1. 根据股票和发布日分组
+    2. 组内没有未来数据
+    3. 组内的同一报告期数据只取最新
 
     """
-    return (
+    TMP = '__TMP__'
+    DATE = '__DATE__'
+
+    # 根据发布日期，笛卡尔扩展，过滤掉未来数据
+    df1 = (
         df
-        .fill_null(np.nan)  # 防止某期无值，forward_fill时产生数据跨期的问题
-        .filter((pl.col(by3).diff() > 0).fill_null(True).over(by1, order_by=[by2, by3]))  # 过滤更新历史报告
-        .sort(by1, by2, by3)
-        .unique(subset=[by1, by2], keep='last', maintain_order=True)  # 一天公布多期，只看最新一期
+        .select(pl.col(by1).alias(TMP), pl.col(by2).alias(DATE))
+        .join_where(df, (pl.col(TMP) == pl.col(by1)) & (pl.col(DATE) >= pl.col(by2)))
+        .drop(TMP)
     )
 
+    # 过滤数据，同报告期只保留最新的一条
+    df2 = (
+        df1
+        .sort(by1, DATE, by3, by2)
+        .unique([by1, DATE, by3], keep="last", maintain_order=True)
+    )
+    return df2
 
-def join_financial(df1: pl.DataFrame, df2: pl.DataFrame, by1: str = 'code', by2: str = 'time', by3: str = 'statDate'):
+
+def pit_calc(df: pl.DataFrame, by1: str = 'stock_code', by2: str = '__DATE__', by3: str = 'report_date') -> pl.DataFrame:
+    """输入PIT分组的财务数据，组内可计算时序指标"""
+    df1 = (
+        df.with_columns(
+            # TODO 补充其他各种时序指标，注意，不要少了`( ).over(by1, by2, order_by=by3)`
+            net_profit_to_total_operate_revenue_ttm=(pl.col('net_profit').rolling_mean(4) / pl.col('total_operating_revenue').rolling_mean(4)).over(by1, by2, order_by=by3)
+        )
+    )
+    return df1
+
+
+def pit_last(df: pl.DataFrame, by1: str = 'stock_code', by2: str = '__DATE__', by3: str = 'report_date') -> pl.DataFrame:
+    """输入PIT分组的财务数据，每组只取最后一条"""
+    df1 = (
+        df
+        .sort(by1, by2, by3)
+        .unique([by1, by2], keep="last", maintain_order=True)
+    )
+    return df1
+
+
+def join_financial(df1: pl.DataFrame, df2: pl.DataFrame, by1: str = 'code', by2: str = 'time'):
     """合并行情与财务表。请提前对财务表进行ttm等计算。因为之后再按报告期对齐很麻烦
 
     1. 同一天发布多期，需要正确排序最新一期
@@ -47,40 +77,26 @@ def join_financial(df1: pl.DataFrame, df2: pl.DataFrame, by1: str = 'code', by2:
     df1:
         行情表
     df2:
-        财务表
+        财务表。收盘后公布会显示第二天，一周7天都可能公布。同一天能公布多期
     by1
     by2
-    by3
 
     """
-    # 收盘后公布会显示第二天，一周7天都可能公布。同一天能公布多期
-    df2 = adjust_financial(df2, by1, by2, by3)
+    df1 = df1.sort(by1, by2)
+    df2 = df2.sort(by1, by2)
+    df3 = df1.join_asof(df2, left_on=by2, right_on=by2, by=by1, strategy="backward", check_sortedness=False)
 
-    # 标记行情表，然后全连接
-    d1 = df1.with_columns(__left__=True).join(df2, on=[by1, by2], how='full', coalesce=True)
-    # 财务数据forward_fill填充
-    d0 = d1.sort(by1, by2).with_columns(
-        pl.col(*df2.columns).forward_fill().over(by1, order_by=by2),
-        # 周五收盘后公布，标记到周六，但没有周一行情承接，但不能直接丢弃，标记一下
-        __last__=pl.col('time').is_null().is_last_distinct().over(by1, order_by=by2),
-    )
-    # xx = d0.filter(pl.col("code") == "002505.XSHE").select("code", "time", "close", "__left__", "__last__")
-    # xx = d0.filter(pl.col('code') == '002721.XSHE', pl.col('time') >= datetime(2023, 4, 20))
-    d0 = d0.filter(pl.col('__left__') | pl.col('__last__'))
-    d0 = d0.drop('__left__', '__last__').fill_nan(None)
-    return d0
+    return df3
 
 
 def step1(ROOT) -> pl.DataFrame:
     PATH_INPUT_1 = rf'{ROOT}\get_fundamentals_balance'
     PATH_INPUT_2 = rf'{ROOT}\get_fundamentals_cash_flow'
     PATH_INPUT_3 = rf'{ROOT}\get_fundamentals_income'
-    # xx1 = load_financial(PATH_INPUT_1).filter(pl.col('code') == '600107.XSHG').sort('code', 'time')
-    # xx2 = load_financial(PATH_INPUT_2).filter(pl.col('code') == '600107.XSHG').sort('code', 'time')
 
-    df1 = adjust_financial(load_financial(PATH_INPUT_1)).with_columns(__mask1__=1)
-    df2 = adjust_financial(load_financial(PATH_INPUT_2)).with_columns(__mask2__=2)
-    df3 = adjust_financial(load_financial(PATH_INPUT_3)).with_columns(__mask4__=4)
+    df1 = load_financial(PATH_INPUT_1).with_columns(__mask1__=1)
+    df2 = load_financial(PATH_INPUT_2).with_columns(__mask2__=2)
+    df3 = load_financial(PATH_INPUT_3).with_columns(__mask4__=4)
 
     df7 = (
         df1
@@ -89,19 +105,19 @@ def step1(ROOT) -> pl.DataFrame:
         .with_columns(__mask7__=pl.sum_horizontal('__mask1__', '__mask2__', '__mask4__'))
         .fill_null(np.nan)
     )
-    except_report = df7.filter(pl.col('__mask7__') < 7)
+    except_report = df7.filter(pl.col('__mask7__') < 7).select('code', 'time', 'statDate', '__mask1__', '__mask2__', '__mask4__', '__mask7__')
     logger.warning('异常基础表 {} 条，请争取补全数据', len(except_report))
 
     PATH_INPUT_4 = rf'{ROOT}\get_fundamentals_indicator'
-    df4 = adjust_financial(load_financial(PATH_INPUT_4)).with_columns(__mask8__=8)
+    df4 = load_financial(PATH_INPUT_4).with_columns(__mask8__=8)
     df15 = (
         df7
         .join(df4, on=['code', 'time', 'statDate'], how='full', coalesce=True)
         .with_columns(__mask15__=pl.sum_horizontal('__mask7__', '__mask8__'))
         .fill_null(np.nan)
     )
-    except_report7 = df15.filter(pl.col('__mask7__').is_nan())
-    except_report8 = df15.filter(pl.col('__mask8__').is_nan())
+    except_report7 = df15.filter(pl.col('__mask7__').is_nan()).select('code', 'time', 'statDate', '__mask7__', '__mask8__', '__mask15__')
+    except_report8 = df15.filter(pl.col('__mask8__').is_nan()).select('code', 'time', 'statDate', '__mask7__', '__mask8__', '__mask15__')
     logger.warning('缺失基础表 {} 条，缺失衍生表 {} 条，请争取补全数据', len(except_report7), len(except_report8))
 
     return df15.drop('__mask1__', '__mask2__', '__mask4__', '__mask7__', '__mask8__', '__mask15__')
@@ -120,10 +136,20 @@ def main():
     df2 = df2.select(cs.all().shrink_dtype())
     df2 = df2.shrink_to_fit()
 
-    # logger.info('start write')
-    # df2.write_parquet(PATH_OUTPUT / 'data3.parquet')
-    # logger.info('done')
-    # print(df2.tail())
+    logger.info('start write')
+    df2.write_parquet(PATH_OUTPUT / 'data3.parquet')
+    logger.info('done')
+    print(df2.tail())
+
+    # ======================================
+    # Point In Time处理+时序指标计算
+
+    df = pit_prepare(df2, by1='code', by2='time', by3='statDate')
+    df = pit_calc(df, by1='code', by2='__DATE__', by3='statDate')
+    df2 = pit_last(df, by1='code', by2='__DATE__', by3='statDate').drop('__DATE__')
+    del df
+
+    # ======================================
 
     # TODO 涉及的字段太多，建议只取自己需要的字段用来合并
     df1 = pl.read_parquet(PATH_OUTPUT / 'data2.parquet')
@@ -136,7 +162,7 @@ def main():
     df3 = df3.shrink_to_fit()
 
     # 分成两个文件，只有最后一个文件需要上传到网络，方便同步数据
-    df3.filter(pl.col('time') < pl.date(2025, 9, 1)).sort("code", "time").write_parquet(PATH_OUTPUT / 'data4.parquet')
+    df3.filter(pl.col('time') < pl.date(2025, 9, 1)).sort("code", "time").write_parquet(PATH_OUTPUT / 'data4_.parquet')
     df3.filter(pl.col('time') >= pl.date(2025, 9, 1)).sort("code", "time").write_parquet(PATH_OUTPUT / 'data5.parquet')
     logger.info('done')
     print(df3.tail())
