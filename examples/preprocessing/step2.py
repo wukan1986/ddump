019f3e5b5f39976fc77024533e798cb904d5f894
wukan <wu-kan@163.com>
2026-01-18 21:47:57 +0800

    重构财务数据

--- examples/preprocessing/step2.py
+++ examples/preprocessing/step2.py
@@ -8,6 +8,7 @@ import polars as pl
 import polars.selectors as cs
 from expr_codegen import codegen_exec
 from loguru import logger
+from polars_ta.utils.factor import calc_factor_muldiv
 from polars_ta.wq import purify
 
 
@@ -36,40 +37,6 @@ def _code_block_2():
     NEXT_DOJI4 = DOJI4[-1]
 
 
-def calc_factor1(df: pl.DataFrame,
-                 by1: str = 'stock_code', by2: str = 'time',
-                 close: str = 'close', pre_close: str = 'pre_close') -> pl.DataFrame:
-    """计算复权因子，乘除法。使用交易所发布的昨收盘价计算
-
-    Parameters
-    ----------
-    df : pl.DataFrame
-        数据
-    by1 : str
-        分组字段
-    by2 : str
-        排序字段
-    close : str
-        收盘价字段
-    pre_close : str
-        昨收盘价字段
-
-    Notes
-    -----
-    不关心是否真发生了除权除息过程，只要知道前收盘价和收盘价不等就表示发生了除权除息
-
-    """
-    df = (
-        df
-        .sort(by1, by2)
-        .with_columns(
-            factor1=(pl.col(close).shift(1, fill_value=pl.first(pre_close)) / pl.col(pre_close)).round(8).over(
-                by1, order_by=by2))
-        .with_columns(factor2=(pl.col('factor1').cum_prod()).over(by1, order_by=by2))
-    )
-    return df
-
-
 def step1(PATH_INPUT1) -> pl.DataFrame:
     # 调整字段名
     df = pl.read_parquet(PATH_INPUT1)
@@ -81,7 +48,7 @@ def step1(PATH_INPUT1) -> pl.DataFrame:
         vwap=pl.col('amount') / pl.col('volume')
     )
 
-    df = calc_factor1(df, by1='code', by2='time', close='close', pre_close='pre_close')
+    df = calc_factor_muldiv(df, by1='code', by2='time', close='close', pre_close='pre_close')
 
     df = df.with_columns(
         # 后复权
@@ -100,7 +67,7 @@ def step1(PATH_INPUT1) -> pl.DataFrame:
 def step2(df: pl.DataFrame) -> pl.DataFrame:
     df = df.fill_nan(None)
 
-    df = codegen_exec(df, _code_block_1, asset='code', date='time', over_null=None)#.filter(pl.col('paused') == 0)
+    df = codegen_exec(df, _code_block_1, asset='code', date='time', over_null=None)  # .filter(pl.col('paused') == 0)
     df = codegen_exec(df, _code_block_2, asset='code', date='time', over_null="partition_by")
 
     df = df.drop(['sz50', 'hs300', 'zz500', 'zz1000', 'zz2000'])
--- examples/preprocessing/step3.py
+++ examples/preprocessing/step3.py
@@ -1,9 +1,9 @@
 import pathlib
 
-import numpy as np
 import polars as pl
 import polars.selectors as cs
 from loguru import logger
+from polars_ta.utils.pit import join_quote_financial, pit_prepare, pit_frist, ANNOUNCE_DATE
 
 
 def load_financial(path: str) -> pl.DataFrame:
@@ -13,39 +13,11 @@ def load_financial(path: str) -> pl.DataFrame:
         .rename({'pubDate': 'time'})
         .drop(['id', 'statDate.1'])
         .with_columns(pl.col('time', 'statDate').str.strptime(pl.Datetime, "%Y-%m-%d"))
-        .fill_null(np.nan)  # 防止某期无值，forward_fill时产生数据跨期的问题
+        # .fill_null(np.nan)  # 防止某期无值，forward_fill时产生数据跨期的问题
     )
 
 
-def pit_prepare(df: pl.DataFrame, by1: str = 'stock_code', by2: str = 'announce_date', by3: str = 'report_date') -> pl.DataFrame:
-    """将原始的财务表根据发布日期重新扩展，满足以下条件：
-
-    1. 根据股票和发布日分组
-    2. 组内没有未来数据
-    3. 组内的同一报告期数据只取最新
-
-    """
-    TMP = '__TMP__'
-    DATE = '__DATE__'
-
-    # 根据发布日期，笛卡尔扩展，过滤掉未来数据
-    df1 = (
-        df
-        .select(pl.col(by1).alias(TMP), pl.col(by2).alias(DATE))
-        .join_where(df, (pl.col(TMP) == pl.col(by1)) & (pl.col(DATE) >= pl.col(by2)))
-        .drop(TMP)
-    )
-
-    # 过滤数据，同报告期只保留最新的一条
-    df2 = (
-        df1
-        .sort(by1, DATE, by3, by2)
-        .unique([by1, DATE, by3], keep="last", maintain_order=True)
-    )
-    return df2
-
-
-def pit_calc(df: pl.DataFrame, by1: str = 'stock_code', by2: str = '__DATE__', by3: str = 'report_date') -> pl.DataFrame:
+def pit_calc(df: pl.DataFrame, by1: str = 'stock_code', by2: str = ANNOUNCE_DATE, by3: str = 'report_date') -> pl.DataFrame:
     """输入PIT分组的财务数据，组内可计算时序指标"""
     df1 = (
         df.with_columns(
@@ -56,40 +28,8 @@ def pit_calc(df: pl.DataFrame, by1: str = 'stock_code', by2: str = '__DATE__', b
     return df1
 
 
-def pit_last(df: pl.DataFrame, by1: str = 'stock_code', by2: str = '__DATE__', by3: str = 'report_date') -> pl.DataFrame:
-    """输入PIT分组的财务数据，每组只取最后一条"""
-    df1 = (
-        df
-        .sort(by1, by2, by3)
-        .unique([by1, by2], keep="last", maintain_order=True)
-    )
-    return df1
-
-
-def join_financial(df1: pl.DataFrame, df2: pl.DataFrame, by1: str = 'code', by2: str = 'time'):
-    """合并行情与财务表。请提前对财务表进行ttm等计算。因为之后再按报告期对齐很麻烦
-
-    1. 同一天发布多期，需要正确排序最新一期
-    2. 更新历史上的某一期，不能把他当成最新一期
-
-    Parameters
-    ----------
-    df1:
-        行情表
-    df2:
-        财务表。收盘后公布会显示第二天，一周7天都可能公布。同一天能公布多期
-    by1
-    by2
-
-    """
-    df1 = df1.sort(by1, by2)
-    df2 = df2.sort(by1, by2)
-    df3 = df1.join_asof(df2, left_on=by2, right_on=by2, by=by1, strategy="backward", check_sortedness=False)
-
-    return df3
-
-
 def step1(ROOT) -> pl.DataFrame:
+    # 全是单季数据
     PATH_INPUT_1 = rf'{ROOT}\get_fundamentals_balance'
     PATH_INPUT_2 = rf'{ROOT}\get_fundamentals_cash_flow'
     PATH_INPUT_3 = rf'{ROOT}\get_fundamentals_income'
@@ -103,7 +43,6 @@ def step1(ROOT) -> pl.DataFrame:
         .join(df2, on=['code', 'time', 'statDate'], how='full', coalesce=True)
         .join(df3, on=['code', 'time', 'statDate'], how='full', coalesce=True)
         .with_columns(__mask7__=pl.sum_horizontal('__mask1__', '__mask2__', '__mask4__'))
-        .fill_null(np.nan)
     )
     except_report = df7.filter(pl.col('__mask7__') < 7).select('code', 'time', 'statDate', '__mask1__', '__mask2__', '__mask4__', '__mask7__')
     logger.warning('异常基础表 {} 条，请争取补全数据', len(except_report))
@@ -114,10 +53,9 @@ def step1(ROOT) -> pl.DataFrame:
         df7
         .join(df4, on=['code', 'time', 'statDate'], how='full', coalesce=True)
         .with_columns(__mask15__=pl.sum_horizontal('__mask7__', '__mask8__'))
-        .fill_null(np.nan)
     )
-    except_report7 = df15.filter(pl.col('__mask7__').is_nan()).select('code', 'time', 'statDate', '__mask7__', '__mask8__', '__mask15__')
-    except_report8 = df15.filter(pl.col('__mask8__').is_nan()).select('code', 'time', 'statDate', '__mask7__', '__mask8__', '__mask15__')
+    except_report7 = df15.filter(pl.col('__mask7__').is_null()).select('code', 'time', 'statDate', '__mask7__', '__mask8__', '__mask15__')
+    except_report8 = df15.filter(pl.col('__mask8__').is_null()).select('code', 'time', 'statDate', '__mask7__', '__mask8__', '__mask15__')
     logger.warning('缺失基础表 {} 条，缺失衍生表 {} 条，请争取补全数据', len(except_report7), len(except_report8))
 
     return df15.drop('__mask1__', '__mask2__', '__mask4__', '__mask7__', '__mask8__', '__mask15__')
@@ -145,8 +83,8 @@ def main():
     # Point In Time处理+时序指标计算
 
     df = pit_prepare(df2, by1='code', by2='time', by3='statDate')
-    df = pit_calc(df, by1='code', by2='__DATE__', by3='statDate')
-    df2 = pit_last(df, by1='code', by2='__DATE__', by3='statDate').drop('__DATE__')
+    df = pit_calc(df, by1='code', by2=ANNOUNCE_DATE, by3='statDate')
+    df2 = pit_frist(df, by1='code', by2='time', by3='statDate', by4=ANNOUNCE_DATE)
     del df
 
     # ======================================
@@ -154,7 +92,7 @@ def main():
     # TODO 涉及的字段太多，建议只取自己需要的字段用来合并
     df1 = pl.read_parquet(PATH_OUTPUT / 'data2.parquet')
     logger.info('start join')
-    df3 = join_financial(df1, df2)
+    df3 = join_quote_financial(df1, df2, by1='code', by2='time')
     del df1
     del df2
 
